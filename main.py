1577c850c07ed3ce43d775e4f3dba7b3c024b8e8
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-11-16 22:45:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from os import path
+from configparser import ConfigParser
+from pyrogram import Client
+from shazamio import Shazam, exceptions, FactoryArtist, FactoryTrack
+
+shazam = Shazam()
+
+from pyrogram import Client
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "6"))
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    bot = Client(
+        "Music-Bot",
+        bot_token=BOT_TOKEN,
+        api_hash=API_HASH,
+        api_id=API_ID,
+        plugins=plugins
+    )
+    bot.run()
